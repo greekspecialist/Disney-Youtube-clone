007ac144e626c140c08fe7caa7b373deaa8f7bd0
greekspecialist <satvisingh2002@gmail.com>
2022-12-06 19:27:27 +0530

    next commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 
 const Header = (props) =>{
-    return <Nav> Header </Nav>;
+    return <Nav> 
+        <Logo><img src="/Images/logo.png" alt ="Disney+"/></Logo>
+        <NavMenu>
+            <a href = '/home'>
+                <img src ='/Images/home-icon.svg' alt = 'Home'/>
+            </a>
+            <span> HOME </span>
+            
+        </NavMenu>
+        </Nav>;
 
 };
 
@@ -21,5 +30,60 @@ letter-spacing: 10px;
 z-index:3;
 
 `;
+
+const Logo = styled.a`
+padding: 0;
+width: 90px;
+margin-top:4px;
+max-height: 70px;
+font-size;
+display: inline-block;
+
+img{
+    display: block;
+    width: 100%;
+}
+`;
+
+const NavMenu = styled.div`
+align-items: center;
+display: flex;
+flex-flow: row nowrap;
+height: 100%;
+margin: 0px;
+position: relative;
+margin-right: auto;
+margin-left: 25px;
+
+a{
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+}
+img{
+    height: 20px;
+    min-width: 20px;
+    width: 20px;
+    z-index: auto;
+    align-item: center;
+
+}
+
+span{
+    color: rgb(249,249,249);
+    font-size: 13px;
+    letter-spacing: 1.42px;
+    line-height: 1.08px;
+    padding: 2px 0px;
+    white-space: nowrap;
+    position: relative;
+}
+
+// @media (max-width: 768px){
+//     display: none;
+// }
+`;
+
+
 
 export default Header;
