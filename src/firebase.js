31b9a3b7b7f234a23f3a47a364c6f7e7bc4e93b9
greekspecialist <satvisingh2002@gmail.com>
2022-12-09 17:29:55 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCdvfVJG_xosF4aoB5cR1saOwtmA52dW94",
+    authDomain: "disneyplus-clone-a0c85.firebaseapp.com",
+    projectId: "disneyplus-clone-a0c85",
+    storageBucket: "disneyplus-clone-a0c85.appspot.com",
+    messagingSenderId: "670410455746",
+    appId: "1:670410455746:web:3dcfdec619c2fbed44995b",
+    measurementId: "G-0LDSQW99TY"
+  };
